60637b2e775ab124e3fe1647084bc27b74f78b77
MaoBA1 <94516860+MaoBA1@users.noreply.github.com>
2022-05-17 12:21:38 +0300

    123

--- controllers/post.js
+++ controllers/post.js
@@ -70,6 +70,111 @@ router.get('/getAllPosts', auth, async(request, response) => {
         })
     })
     
+});
+
+router.put('/likePost/:postId', auth, async(request, response) => {
+    const accountId = request.account._id;
+    const postId = request.params.postId;
+    await User.findById(accountId)
+    .then(async account => {
+        if(account) {
+            await Post.findById(postId)
+            .then(async post => {
+                if(post) {
+                    const likeArray = post.likes;
+                    let flag = false;
+                    likeArray.forEach(like => {
+                        if(like.type.toString() == accountId.toString()) {
+                            console.log('test');
+                            flag = true;
+                            return;
+                        }
+                    })
+                    if(!flag) {
+                        likeArray.push(accountId);
+                        post.likes = likeArray;
+                        return post.save()
+                        .then(updated_post => {
+                            return response.status(200).json({
+                                status: true,
+                                Post: updated_post
+                            })
+                        })
+                        .catch(error => {
+                            return response.status(500).json({
+                                status: false,
+                                Error: error
+                            })
+                        });
+                    } else {
+                        return response.status(200).json({
+                            status: false,
+                            message: 'You have already like this post'
+                        })
+                    }
+                    
+                } 
+            })
+        } else {
+            return response.status(403).json({
+                status: false,
+                message: 'User Not Found'
+            })
+        }
+    })
+    .catch(error => {
+        return response.status(500).json({
+            status: false,
+            Error: error
+        })
+    })
+})
+
+
+router.put('/unlikePost/:postId', auth, async(request, response) => {
+    const accountId = request.account._id;
+    const postId = request.params.postId;
+    await User.findById(accountId)
+    .then(async account => {
+        if(account) {
+            await Post.findById(postId)
+            .then(async post => {
+                if(post) {
+                    let likeArray = post.likes.filter(like => like.toString() != accountId.toString());
+                    post.likes = likeArray;
+                    return post.save()
+                    .then(async updated_post => {
+                        return response.status(200).json({
+                            status: true,
+                            Post: updated_post
+                        })
+                    })
+                    .catch(error => {
+                        return response.status(500).json({
+                            status: false,
+                            Error: error
+                        })
+                    })
+                } else {
+                    return response.status(403).json({
+                        status: false,
+                        message: 'Post Not Found'
+                    })
+                }
+            })
+        } else {
+            return response.status(403).json({
+                status: false,
+                message: 'User Not Found'
+            })
+        }
+    })
+    .catch(error => {
+        return response.status(500).json({
+            status: false,
+            Error: error
+        })
+    })
 })
 
 module.exports = router;
