77a388d86ca6b92bdc0eb32e7a07875d5ae67221
MaoBA1 <94516860+MaoBA1@users.noreply.github.com>
2023-01-21 05:34:49 +0200

    123

--- twilio.js
+++ twilio.js
@@ -1,10 +1,10 @@
 const accountSid = "ACbf4683dcb4e1cf33335af721fdc7259e";
-const authToken = "9402085b756e1f98e3b23008a8a61101";
+const authToken = "2ae1ecbdf73055cec3e6bb021f3419be";
 const client = require('twilio')(accountSid, authToken);
 
 const setOptionsAndSenSMS = (phoneNumber, firstName, passcode) => {
     let formattedPhoneNuber = phoneNumber.slice(1, phoneNumber.length);
-    formattedPhoneNuber = `+972${formattedPhoneNuber}`;
+    formattedPhoneNuber = `+972${phoneNumber}`;
     console.log(phoneNumber, firstName, passcode);
     console.log(formattedPhoneNuber);
     client.messages
@@ -13,7 +13,7 @@ const setOptionsAndSenSMS = (phoneNumber, firstName, passcode) => {
         from: '+12517149679',
         to: formattedPhoneNuber
     })
-    .then(message => console.log(message + " massage has sent"))
+    .then(message => console.log(message.direction + " massage has sent"))
     .catch(err => { console.log(err.message); });
 }
 
